ac5de73831aeeee44766908587578e9b5c9c12a3
Grant <justanormalowl@gmail.com>
2024-06-04 14:28:40 -0500

    reactstrap init / site title change

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootswatch/dist/materia/bootstrap.css';
 import SphinxMenu from './SphinxMenu';
 import reportWebVitals from './reportWebVitals';
 
